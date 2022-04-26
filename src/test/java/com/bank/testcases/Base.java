5846c12dc4d4814fedce7af19f9b1886699c2ce5
Sandesh <ersandesh11@gmail.com>
2022-04-27 01:38:41 +0530

    new2

---
+++ src/test/java/com/bank/testcases/Base.java
@@ -0,0 +1,100 @@
+package com.bank.testcases;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import com.bank.Utils.ReadConfig;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+	ReadConfig read = new ReadConfig();
+	
+	public static WebDriver driver;
+    public  String url= read.getApplicationUrl();
+	public  String username= read.getUsername();
+	public  String password= read.getPassword();
+	public static Logger log;
+	
+	
+	@Parameters ("browser")
+	@BeforeMethod
+	public void initilization(String browser) {
+		// System.setProperty("webdriver.chrome.driver",
+		// System.getProperty(("user.dir")+ ""))
+
+		//String browser = "chrome";
+	
+		if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",read.getChromePath());
+			driver = new ChromeDriver();
+			System.out.println("Browser name :"+ browser);
+			System.out.println(Thread.currentThread().getId());
+		}
+		else if (browser.equalsIgnoreCase("ff")) {
+			System.setProperty("webdriver.gecko.driver",read.getFirefoxPath());
+			driver = new FirefoxDriver();
+			System.out.println("Browser name :"+ browser);
+			System.out.println(Thread.currentThread().getId());
+		}
+		else if (browser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			System.out.println("Browser name :"+ browser);
+			System.out.println(Thread.currentThread().getId());
+		}
+	
+	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().deleteAllCookies();
+		driver.get(url);
+		log=org.apache.log4j.Logger.getLogger(Base.class);
+		PropertyConfigurator.configure("Log4j.properties");
+	  
+	
+	}
+	public void captureScreen(WebDriver driver,String tname)   {
+		TakesScreenshot shoot=(TakesScreenshot)driver;
+		File src = shoot.getScreenshotAs(OutputType.FILE);
+		File dec =new File(System.getProperty("user.dir")+"/Screenshoot/"+ tname +".png");
+		try {
+			FileUtils.copyFile(src, dec);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Screenshoot taken");
+		
+	}
+	public String randomString() {
+	 String random=RandomStringUtils.randomAlphabetic(6);
+	 return random;
+	}
+	public String randomNumber() {
+		 String random=RandomStringUtils.randomNumeric(6);
+		 return random;
+		}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+
+}
